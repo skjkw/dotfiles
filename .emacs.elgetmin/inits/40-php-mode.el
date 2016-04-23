29d3e50a9604d70818d062caa413d1865de18b3f
shun.kajikawa <shun.kajikawa@gmail.com>
2016-04-23 23:58:02 +0900

    emacsd updated

--- .emacs.elgetmin/inits/40-php-mode.el
+++ .emacs.elgetmin/inits/40-php-mode.el
@@ -2,7 +2,6 @@
   :config
   (setq php-serch-url "http://jp.php.net/ja/")
   (setq php-manual-url "http://jp.php.net/manual/ja/")
-  (use-package php-extras)
   (use-package php-auto-yasnippets
     :config
     (setq php-auto-yasnippet-php-program (format "%s/php-auto-yasnippets/Create-PHP-YASnippet.php" el-get-dir))
@@ -19,6 +18,20 @@
   (use-package flycheck
     :config
     (flycheck-mode t)
+    )
+
+  (use-package ac-php
+    :config
+    (setq ac-sources '(ac-source-filename
+                       ac-source-functions
+                       ac-source-variables
+                       ac-source-symbols
+                       ac-source-abbrev
+                       ac-source-words-in-same-mode-buffers
+                       ac-source-dictionary
+                       ac-source-php))
+    (define-key php-mode-map  (kbd "C-]") 'ac-php-find-symbol-at-point) ;goto define
+    (define-key php-mode-map  (kbd "C-t") 'ac-php-location-stack-back   ) ;go back
     )
 
   ;; php-mode で削除されるので再定義
